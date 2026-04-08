ec0839a18336e78d73f8280bbb4b1a455b9980a5
Abhijith Sivadas <137138525+xm343@users.noreply.github.com>
2026-04-08 12:13:14 +0530

    Time: 7 ms (30.79%), Space: 60.7 MB (75.37%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
+   let obj = {
+     I:1,
+     V:5,
+     X:10,
+     L:50,
+     C:100,
+     D:500,
+     M:1000
+   }
+   let total = 0
+   for(let i=0; i<s.length; i++){
+    let current = obj[s[i]]
+    let next = obj[s[i+1]]
+    if(current<next){
+        total-=current
+    }
+    else{
+        total+=current
+    }
+   } 
+   return total
+};
